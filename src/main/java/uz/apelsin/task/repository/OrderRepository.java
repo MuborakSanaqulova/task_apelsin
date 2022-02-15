378ea383206e53222a68c1ec95285392596a5cfd
MuborakSanaqulova <bangtangirl0109@gmail.com>
2022-02-15 10:18:02 +0500

    TaskApplication

--- src/main/java/uz/apelsin/task/repository/OrderRepository.java
+++ src/main/java/uz/apelsin/task/repository/OrderRepository.java
@@ -17,4 +17,5 @@ public interface OrderRepository extends JpaRepository<Orders, Integer> {
 
     @Query(value = "select o.id as orderId, o.date as orderDate, (d.quantity * p.price) as totalPrice, p.price as unitPrice, d.quantity as quantity  from  orders o left join invoice i on o.id = i.orders_id join detail d on o.id = d.orders_id join product p on p.id = d.product_id where i.orders_id is null", nativeQuery = true)
     List<OrdersWithoutInvoicesProjection> getOrdersWithoutInvoices();
+
 }
